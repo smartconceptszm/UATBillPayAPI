218a5e0e4835d7f9d7e2c0e48f2f782cd575bedf
Smart Concepts <info@smartconcepts.co.zm>
2024-01-16 16:30:36 +0200

    20240116 002

--- app/Http/Controllers/Auth/UserController.php
+++ app/Http/Controllers/Auth/UserController.php
@@ -30,7 +30,7 @@ class UserController extends Controller
 	{
 
 		try {
-			Log::info('(EFECTIVO)  '.\json_encode($request->query()));
+			Log::info('(EFECTIVO querry params)  '.\json_encode($request->query()));
 			$this->response['data'] = $this->theService->findAll($request->query());
 		} catch (\Throwable $e) {
 				$this->response['status']['code'] = 500;
