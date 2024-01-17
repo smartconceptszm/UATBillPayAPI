62f1b0df618d79606b0b23c654ef4ef87e063df4
Smart Concepts <info@smartconcepts.co.zm>
2024-01-17 13:48:57 +0200

    20240117 001

--- app/Http/Services/MoMo/Utility/StepService_AddShotcutMessage.php
+++ app/Http/Services/MoMo/Utility/StepService_AddShotcutMessage.php
@@ -22,7 +22,7 @@ class StepService_AddShotcutMessage
 							'client_id'=>$momoDTO->client_id,
 							'mobileNumber'=>$momoDTO->mobileNumber
 						]);
-			if ($customer->count() == 0) {
+			if ($customer) {
 				//Create Record
 				$this->shortcutCustomerService->create([
 						'client_id'=> $momoDTO->client_id,
