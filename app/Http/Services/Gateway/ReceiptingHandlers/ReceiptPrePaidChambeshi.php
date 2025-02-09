618de6f0d57eb1b1266f972da1bfefa08fdb979c
Smart Concepts <info@smartconcepts.co.zm>
2025-02-09 18:59:34 +0200

    20250209 003

--- app/Http/Services/Gateway/ReceiptingHandlers/ReceiptPrePaidChambeshi.php
+++ app/Http/Services/Gateway/ReceiptingHandlers/ReceiptPrePaidChambeshi.php
@@ -37,40 +37,46 @@ class ReceiptPrePaidChambeshi implements IReceiptPayment
 									];
 			$tokenResponse=$this->billingClient->generateToken($tokenParams);
 			if($tokenResponse['status']=='SUCCESS'){
+				$paymentDTO->paymentStatus = PaymentStatusEnum::Receipted->value;
 				$paymentDTO->tokenNumber = $tokenResponse['tokenNumber'];
-				$paymentDTO->paymentStatus = PaymentStatusEnum::Paid->value;
+				$paymentDTO->receiptNumber =  $paymentDTO->customerAccount."_".\now()->timestamp;
+				$paymentDTO->receipt = "\n"."Payment successful"."\n".
+											"Amount: ZMW " . \number_format( $paymentDTO->receiptAmount, 2, '.', ',') . "\n".
+											"Acc/Meter No: " . $paymentDTO->customerAccount . "\n" .
+											"Token: ". $paymentDTO->tokenNumber . "\n".
+											"Date: " . Carbon::now()->format('d-M-Y') . "\n";
 			}else{
 				$paymentDTO->error = $tokenResponse['error'];
 			}
 		}
 
-		if(($paymentDTO->tokenNumber != '') && ($paymentDTO->paymentStatus == PaymentStatusEnum::Paid->value)){
-			$receiptNumber =  $paymentDTO->customerAccount."_".\now()->timestamp;
-			$receiptingParams = [
-											"payment_provider"=> strtolower($paymentDTO->walletHandler).'_money',
-											"payer_msisdn"=> $paymentDTO->mobileNumber, 
-											"txnDate"=> Carbon::now()->format('Y-m-d'),
-											"account"=> $paymentDTO->customerAccount,  
-											"amount" => $paymentDTO->receiptAmount, 
-											"txnId" => $paymentDTO->transactionId,
-											"client_id"=> $paymentDTO->client_id,
-											"ReceiptNo"=> $receiptNumber
-										];
-			$billingResponse = $this->billingClient->postPayment($receiptingParams);
-			if($billingResponse['status'] == 'SUCCESS'){
-				$paymentDTO->paymentStatus = PaymentStatusEnum::Receipted->value;
-				$paymentDTO->receiptNumber =  $receiptNumber;
-				$paymentDTO->receipt = "\n"."Payment successful"."\n".
-											"Rcpt No: " . $paymentDTO->receiptNumber . "\n" .
-											"Amount: ZMW " . \number_format( $paymentDTO->receiptAmount, 2, '.', ',') . "\n".
-											"Acc: " . $paymentDTO->customerAccount . "\n".
-											"Token: ". $paymentDTO->tokenNumber . "\n".
-											"Date: " . Carbon::now()->format('d-M-Y') . "\n";
-			}else{
-				$paymentDTO->error = "At receipt payment. ".$billingResponse['error'];
-				$paymentDTO->receiptNumber =  '';
-			}
-		}
+		// if(($paymentDTO->tokenNumber != '') && ($paymentDTO->paymentStatus == PaymentStatusEnum::Paid->value)){
+		// 	$receiptNumber =  $paymentDTO->customerAccount."_".\now()->timestamp;
+		// 	$receiptingParams = [
+		// 									"payment_provider"=> strtolower($paymentDTO->walletHandler).'_money',
+		// 									"payer_msisdn"=> $paymentDTO->mobileNumber, 
+		// 									"txnDate"=> Carbon::now()->format('Y-m-d'),
+		// 									"account"=> $paymentDTO->customerAccount,  
+		// 									"amount" => $paymentDTO->receiptAmount, 
+		// 									"txnId" => $paymentDTO->transactionId,
+		// 									"client_id"=> $paymentDTO->client_id,
+		// 									"ReceiptNo"=> $receiptNumber
+		// 								];
+		// 	$billingResponse = $this->billingClient->postPayment($receiptingParams);
+		// 	if($billingResponse['status'] == 'SUCCESS'){
+		// 		$paymentDTO->paymentStatus = PaymentStatusEnum::Receipted->value;
+		// 		$paymentDTO->receiptNumber =  $receiptNumber;
+		// 		$paymentDTO->receipt = "\n"."Payment successful"."\n".
+		// 									"Rcpt No: " . $paymentDTO->receiptNumber . "\n" .
+		// 									"Amount: ZMW " . \number_format( $paymentDTO->receiptAmount, 2, '.', ',') . "\n".
+		// 									"Acc: " . $paymentDTO->customerAccount . "\n".
+		// 									"Token: ". $paymentDTO->tokenNumber . "\n".
+		// 									"Date: " . Carbon::now()->format('d-M-Y') . "\n";
+		// 	}else{
+		// 		$paymentDTO->error = "At receipt payment. ".$billingResponse['error'];
+		// 		$paymentDTO->receiptNumber =  '';
+		// 	}
+		//}
 		return $paymentDTO;
 
 	}
