c822605fdc2f0497d795cd0a06e88e32b6203494
Smart Concepts <info@smartconcepts.co.zm>
2025-09-21 18:49:42 +0200

    20250921 002

--- app/Http/Services/Gateway/Utility/StepService_ProcessPromotion.php
+++ app/Http/Services/Gateway/Utility/StepService_ProcessPromotion.php
@@ -22,30 +22,32 @@ class StepService_ProcessPromotion
    {
 
       try {
-         $clientWallet = $this->clientWalletService->findById($paymentDTO->wallet_id);
-         $activePromotion = $this->promotionService->findActivePromotion($clientWallet->client_id, $paymentDTO->consumerType);
-         if($activePromotion){
-            $promotionDTO = $this->promotionDTO->fromArray($paymentDTO->toArray());
-            $promotionDTO->promotionRaffleEntryMessage = $activePromotion->raffleEntryMessage;
-            $promotionDTO->promotionRaffleEntryAmount = $activePromotion->raffleEntryAmount;
-            $promotionDTO->promotionConsumerType = $activePromotion->consumerType;
-            $promotionDTO->promotionEntryMessage = $activePromotion->entryMessage;
-            $promotionDTO->promotionEntryAmount = $activePromotion->entryAmount;
-            $promotionDTO->promotionRateValue = $activePromotion->rateValue;
-            $promotionDTO->receiptNumber = $paymentDTO->receiptNumber;
-            $promotionDTO->promotionOnDebt = $activePromotion->onDebt;
-            $promotionDTO->consumerType = $paymentDTO->consumerType;
-            $promotionDTO->client_id = $activePromotion->client_id;
-            $promotionDTO->promotionName = $activePromotion->name;
-            $promotionDTO->promotionType = $activePromotion->type;
-            $promotionDTO->promotion_id = $activePromotion->id;
-            $promotionDTO->menu_id = $paymentDTO->menu_id;
-            $promotionDTO->mno_id = $paymentDTO->mno_id;
-            $promotionDTO->payment_id = $paymentDTO->id;
+         if ($paymentDTO->consumerType){
+            $clientWallet = $this->clientWalletService->findById($paymentDTO->wallet_id);
+            $activePromotion = $this->promotionService->findActivePromotion($clientWallet->client_id, $paymentDTO->consumerType);
+            if($activePromotion){
+               $promotionDTO = $this->promotionDTO->fromArray($paymentDTO->toArray());
+               $promotionDTO->promotionRaffleEntryMessage = $activePromotion->raffleEntryMessage;
+               $promotionDTO->promotionRaffleEntryAmount = $activePromotion->raffleEntryAmount;
+               $promotionDTO->promotionConsumerType = $activePromotion->consumerType;
+               $promotionDTO->promotionEntryMessage = $activePromotion->entryMessage;
+               $promotionDTO->promotionEntryAmount = $activePromotion->entryAmount;
+               $promotionDTO->promotionRateValue = $activePromotion->rateValue;
+               $promotionDTO->receiptNumber = $paymentDTO->receiptNumber;
+               $promotionDTO->promotionOnDebt = $activePromotion->onDebt;
+               $promotionDTO->consumerType = $paymentDTO->consumerType;
+               $promotionDTO->client_id = $activePromotion->client_id;
+               $promotionDTO->promotionName = $activePromotion->name;
+               $promotionDTO->promotionType = $activePromotion->type;
+               $promotionDTO->promotion_id = $activePromotion->id;
+               $promotionDTO->menu_id = $paymentDTO->menu_id;
+               $promotionDTO->mno_id = $paymentDTO->mno_id;
+               $promotionDTO->payment_id = $paymentDTO->id;
 
-            ProcessPromotionJob::dispatch($promotionDTO)
-                                 ->delay(Carbon::now()->addSeconds(20))
-                                 ->onQueue('low');
+               ProcessPromotionJob::dispatch($promotionDTO)
+                                    ->delay(Carbon::now()->addSeconds(20))
+                                    ->onQueue('low');
+            }
          }
       } catch (\Throwable $e) {
          $paymentDTO->error = 'At process promotion. ' . $e->getMessage();
